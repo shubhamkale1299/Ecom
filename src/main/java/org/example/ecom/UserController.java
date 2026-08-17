8c72fd12016d427f56f030b345a48bc0bf453dd7
ShubhamKale <shubhamkale1299@gmail.com>
2026-08-17 12:06:33 +0530

    SECOND COMMIT

--- src/main/java/org/example/ecom/UserController.java
+++ src/main/java/org/example/ecom/UserController.java
@@ -4,16 +4,17 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class UserController {
 
+ public List<User> userList = new ArrayList<>();
 
-
-        @GetMapping("/api/users")
-        public List<User> getAllUsers() {
-            return userList;
-        }
+    @GetMapping("/api/users")
+    public List<User> getAllUsers() {
+        return userList;
+    }
 
         @PostMapping("/api/users")
         public List<User> createUser(@RequestBody User user) {
@@ -22,4 +23,4 @@ public class UserController {
         }
     }
 
-}
+
